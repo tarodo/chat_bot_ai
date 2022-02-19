da0f3d866f3985fd29d112faff7fc923d476ec0c
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-02-19 17:57:48 +0300

    set logger

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 import logging
 import os
 
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from environs import Env
 from google.cloud import dialogflow
 
-logger = logging.getLogger("homework")
-logger.setLevel(logging.DEBUG)
 
-strfmt = "[%(asctime)s] [%(name)s] [%(levelname)s] > %(message)s"
-datefmt = "%Y-%m-%d %H:%M:%S"
-formatter = logging.Formatter(fmt=strfmt, datefmt=datefmt)
+class MyLogsHandler(logging.Handler):
+    def __init__(self, tg_token: str, chat_id: str):
+        super().__init__()
+        self.token = tg_token
+        self.chat_id = chat_id
 
-handler_st = logging.StreamHandler()
-handler_st.setFormatter(formatter)
+    def emit(self, record):
+        log_bot = Bot(token=self.token)
+        log_bot.send_message(self.chat_id, self.format(record))
+
+
+def get_logger(logger_name, bot_reporter_token, chat_id):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+
+    strfmt = "[%(asctime)s] [%(name)s] [%(levelname)s] > %(message)s"
+    datefmt = "%Y-%m-%d %H:%M:%S"
+    formatter = logging.Formatter(fmt=strfmt, datefmt=datefmt)
+
+    handler_st = logging.StreamHandler()
+    handler_st.setFormatter(formatter)
+
+    if bot_reporter_token and chat_id:
+        strfmt = "%(asctime)s :: %(levelname)s :: %(message)s"
+        datefmt = "%d.%m %H:%M:%S"
+        formatter = logging.Formatter(fmt=strfmt, datefmt=datefmt)
+
+        handler_tg = MyLogsHandler(bot_reporter_token, chat_id)
+        handler_tg.setFormatter(formatter)
+
+        logger.addHandler(handler_st)
+        logger.addHandler(handler_tg)
+
+    return logger
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -60,7 +86,7 @@ def main(bot_token) -> None:
 if __name__ == "__main__":
     env = Env()
     env.read_env()
-
+    logger = get_logger("tg_bot", os.getenv("BOT_REPORT_TOKEN"), os.getenv("CHAT_ID"))
     bot_token = os.environ["BOT_TOKEN"]
     PROJECT_ID = os.environ["GOOGLE_PROJECT_ID"]
     main(bot_token)
--- vk_bot.py
+++ vk_bot.py
@@ -4,11 +4,12 @@ import random
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
-from main import detect_intent_text
+from main import detect_intent_text, get_logger
 
 
 def dialogflow_answer(event, vk_api):
     response = detect_intent_text(PROJECT_ID, event.user_id, event.text)
+
     if response:
         vk_api.messages.send(
             user_id=event.user_id,
@@ -21,6 +22,8 @@ if __name__ == "__main__":
     env = Env()
     env.read_env()
     PROJECT_ID = os.environ["GOOGLE_PROJECT_ID"]
+    logger = get_logger("vk_bot", os.getenv("BOT_REPORT_TOKEN"), os.getenv("CHAT_ID"))
+
     vk_session = vk_api.VkApi(token=os.environ["VK_BOT_TOKEN"])
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
